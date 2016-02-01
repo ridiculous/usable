4e1d4976fa90fb6d59d5f7272af6e9ec95246bbe
Ryan Buckley <arebuckley@gmail.com>
2016-02-01 00:22:30 -0800

    update specs

--- spec/lib/usable_spec.rb
+++ spec/lib/usable_spec.rb
@@ -3,7 +3,11 @@ require "spec_helper"
 describe Usable do
   module Versionable
     def versions
-      "Saving #{self.class.config.max_versions} versions to #{self.class.config.table_name}"
+      "Saving #{self.class.config.max_versions} versions to #{self.class.config.table_name} table"
+    end
+
+    def destroy_version
+      "destroying version"
     end
   end
 
@@ -33,6 +37,13 @@ describe Usable do
           end
         }.to change(subject.config, :max_versions).from(nil).to(10)
       end
+
+      it "uses the config in the method calls" do
+        subject.usable mod, table_name: 'versions' do |config|
+          config.max_versions = 10
+        end
+        expect(subject.new.versions).to eq "Saving 10 versions to versions table"
+      end
     end
 
     context "not block_given? we assume it's a hash" do
@@ -40,6 +51,25 @@ describe Usable do
         expect {
           subject.usable Versionable, table_name: 'custom'
         }.to change(subject.config, :table_name).from(nil).to('custom')
+      end
+    end
+
+    context 'when given the :only option' do
+      it 'defines the specified method' do
+        subject.usable Versionable, only: :destroy_version
+        expect(subject.new.destroy_version).to eq "destroying version"
+      end
+
+      it 'defines the specified methods' do
+        subject.usable Versionable, only: [:destroy_version, :versions]
+        expect(subject.new.destroy_version).to eq "destroying version"
+        expect(subject.new.versions).to match(/saving/i)
+      end
+
+      it 'stubs out the other methods to return nil' do
+        subject.usable Versionable, only: :destroy_version
+        expect(subject.new.destroy_version).to eq "destroying version"
+        expect(subject.new.versions).to be_nil
       end
     end
   end
